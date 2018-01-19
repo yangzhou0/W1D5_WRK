a5100c5054d1d0d730f5435bb295d2dafc9fd10c
Yang & Jacob <yzhou3991@gmail.com>
2018-01-19 14:02:35 -0500

    Add poly tree

---
+++ poly_tree_node/lib/00_tree_node.rb
@@ -0,0 +1,31 @@
+require 'byebug'
+class PolyTreeNode
+  attr_reader :children, :value, :parent
+  def initialize (value)
+    @value = value
+    @parent = nil 
+    @children = []
+  end
+
+  def parent=(node)
+    if parent != nil
+      parent.children.delete(self)
+    end
+    @parent = node
+    parent.children.push(self) unless parent == nil || parent.children.include?(self)
+  end
+  
+  def add_child(child_node)
+    children.push(child_node)
+    child_node.parent = self
+  end
+  
+  def remove_child(child_node)
+    if self.children.include?(child_node)
+      children.delete(child_node)
+      child_node.parent = nil
+    else
+      raise "Node does not have this child."
+    end
+  end
+end
